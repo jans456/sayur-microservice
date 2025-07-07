55871e08967789cc0e9cd95c3a2de902b4e6efe7
sobatguruntech <105582665+jans456@users.noreply.github.com>
2025-07-07 21:55:11 +0700

    Create user service

---
+++ user-service/internal/core/service/user_service.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"user-service/internal/adapter/repository"
+	"user-service/internal/core/domain/entity"
+	"user-service/utils/conv"
+
+	"github.com/labstack/gommon/log"
+)
+
+type UserServiceInterface interface {
+	SignIn(ctx context.Context, req entity.UserEntity) (*entity.UserEntity, string, error)
+}
+
+type userService struct {
+	repo repository.UserRepositoryInterface
+}
+
+// SignIn implements UserServiceInterface.
+func (u *userService) SignIn(ctx context.Context, req entity.UserEntity) (*entity.UserEntity, string, error) {
+	user, err := u.repo.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		log.Errorf("[User-Servie-1] SignIn:  %v", err)
+		return nil, "", err
+	}
+
+	if checkPass := conv.CheckPasswordHash(req.Password, user.Password); !checkPass {
+		err = errors.New("password is incorrect")
+		log.Errorf("[User-Servie-1] SignIn:  %v", err)
+		return nil, "", err
+	}
+
+	return user, "", nil
+}
+
+func NewUserService(repo repository.UserRepositoryInterface) UserServiceInterface {
+	return &userService{repo: repo}
+}
