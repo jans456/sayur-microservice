adfcd793f6993468d2a107f09b2b814ba38dab9e
sobatguruntech <105582665+jans456@users.noreply.github.com>
2025-07-07 17:20:12 +0700

    create user repository
    Co-Authored-By: Godbless <79393422+jans45@users.noreply.github.com>

---
+++ user-service/internal/adapter/repository/user_repository.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"context"
+	// "log"
+	"user-service/internal/core/domain/entity"
+	"user-service/internal/core/domain/model"
+
+	"github.com/labstack/gommon/log"
+
+	"gorm.io/gorm"
+)
+
+type UserRepositoryInterface interface {
+	GetUserByEmail(ctx context.Context, email string) (*entity.UserEntity, error)
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+// Get User By Email implements UserRepositoryInterface
+func (u *userRepository) GetUserByEmail(ctx context.Context, email string) (*entity.UserEntity, error) {
+	modelUser := model.User{}
+
+	if err := u.db.Where("email = ? && is_verified = ?", email, true).
+		Preload("Roles").First(&modelUser).Error; err != nil {
+			log.Errorf("[UserRepository-1] GetUserByEmail: %v", err)
+			return nil, err
+	}
+
+
+	return &entity.UserEntity{
+		ID:			modelUser.ID,
+		Name:		modelUser.Name,
+		Email:		email,
+		Password:	modelUser.Password,
+		RoleName:	modelUser.Roles[0].Name,
+		Address:	modelUser.Address,
+		Phone:		modelUser.Phone,
+		Photo:		modelUser.Photo,
+		Lat:		modelUser.Lat,
+		Lng:		modelUser.Lng,
+		IsVerified:	modelUser.IsVerified,
+	}, nil
+}
+
+func NewUserRepository(db *gorm.DB) UserRepositoryInterface {
+	return &userRepository{db: db}
+}
