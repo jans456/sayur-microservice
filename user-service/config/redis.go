f681ead0adbb797f932fdc7de59679aba8ce9967
sobatguruntech <105582665+jans456@users.noreply.github.com>
2025-07-15 08:41:33 +0700

    create redist

--- user-service/config/redis.go
+++ user-service/config/redis.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var Ctx = context.Background()
+
+func NewRedisClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: "redis-user:6379", // Ganti sesuai dengan pengaturan Docker atau lokal
+	})
+
+	// Memastikan Redis terhubung
+	_, err := client.Ping(Ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	return client
+}
